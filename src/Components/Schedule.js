0f046f54010c0ef0d7e447917465f801867f6451
LukasLindqvist <lukas.lindqvist@student.kyh.se>
2022-06-09 19:53:13 +0200

    added sprint race to schedule

--- src/Components/Schedule.js
+++ src/Components/Schedule.js
@@ -26,9 +26,9 @@ function Schedule() {
         })
     },[])
 
+    
 
-
-    let scheduleOutput = 
+    let raceSchedule = 
     //remove race weekends with sprint races as these dont have a practice 3
     data.filter(function( data ) {
         if (data.ThirdPractice === undefined) {  
@@ -64,7 +64,7 @@ function Schedule() {
                                 </div>
                                 <div className='sessionText'>
                                     <h3>QUALYFYING</h3>
-                                    <h4>FRI</h4>
+                                    <h4>SAT</h4>
                                     <h5>{data.Qualifying.time.slice(0,5)}</h5>
                                 </div>
                             <div className='line'></div>
@@ -83,15 +83,75 @@ function Schedule() {
         </React.Fragment>
         )
     })
+    
 
+    let sprintRaceOutput =
+     //remove race weekends without sprint race as these do have a third practice session
+    data.filter(function( data ) {
+        if (data.ThirdPractice !== undefined) {  
+            return false 
+        }
+            return true
 
+    })
+    .map((data) => {      
+        return(
+            <React.Fragment key ={data.round}>
+            <div className='contentContainer'>
+            <div className='scheduleContainer'>
+                <div className='scheduleRace'  style={{backgroundImage: `url(${Cover})`}} >
+                    <img src={Country} alt='country flag' id='flagImg'></img>
+                    <img src={Trackmap} alt='Trackmap' id='trackMapImg'></img>
+                    <div className='sessionInfo'>
+                        <div className='sessionText'>
+                            <h3>PRACTICE 1</h3>
+                            <h4>FRI</h4>
+                            <h5>{data.round}</h5>
+                        </div>
+                        <div className='sessionText'>
+                            <h3>QUALYFYING</h3>
+                            <h4>FRI</h4>
+                            <h5>{data.Qualifying.time.slice(0,5)}</h5>
+                        </div>
+                            <div className='line'></div>
+                                <div className='sessionText'>
+                                    <h3>PRACTICE 2</h3>
+                                    <h4>SAT</h4>
+                                    <h5>{data.SecondPractice.time.slice(0,5)}</h5>
+                                </div>
+                                <div className='sessionText'>
+                                    <h3>SPRINT RACE</h3>
+                                    <h4>SAT</h4>
+                                    <h5>{data.Sprint.time.slice(0,5)}</h5>
+                                </div>
+                            <div className='line'></div>
+                                <div className='sessionText'>
+                                    <h3>RACE</h3>
+                                    <h4>SUN</h4>
+                                    <h5>{data.time.slice(0,5)}</h5>
+                                </div>
+                                <div className='sessionText'>
+                                    <p>Session start time based on GMT + 0</p>
+                                </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </React.Fragment>
+        )
+    })
 
+    //add the sprint races into the correct position in schedule 
+    raceSchedule.splice(3,0, sprintRaceOutput[0])
+    raceSchedule.splice(10,0, sprintRaceOutput[1])
+    raceSchedule.splice(20,0, sprintRaceOutput[2])
     return (
         <>
+        {sprintRaceOutput}
          <div className='title'>
             <h2>Schedule</h2>
         </div>
-        {scheduleOutput}    
+        {raceSchedule}    
         </>
         
     
